85be2cc0af6e7a1bdf0c542e156a27b1991a0c06
ArturPawelski <arturpawelski13@gmail.com>
2024-03-28 20:10:54 +0100

    loader and Toast

---
+++ src/Components/UI/ToastMessage.ts
@@ -0,0 +1,29 @@
+import { useToast } from '@chakra-ui/react';
+
+export const useToastNotifications = () => {
+  const toast = useToast();
+
+  const successToast = (description: string, title: string = 'Success') => {
+    toast({
+      title,
+      description,
+      status: 'success',
+      duration: 9000,
+      isClosable: true,
+      position: 'top',
+    });
+  };
+
+  const errorToast = (description: string, title: string = 'Error') => {
+    toast({
+      title,
+      description,
+      status: 'error',
+      duration: 9000,
+      isClosable: true,
+      position: 'top',
+    });
+  };
+
+  return { successToast, errorToast };
+};
