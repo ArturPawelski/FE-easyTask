d51bb2c3ced1ac9a913784ede463c116d57ae9b2
ArturPawelski <arturpawelski13@gmail.com>
2024-04-29 15:10:10 +0200

    protect route test

--- src/Tests/ProtectedRoute.test.tsx
+++ src/Tests/ProtectedRoute.test.tsx
@@ -1,24 +1,56 @@
-import { describe, it, expect, vi } from 'vitest';
-import { render, screen } from '@testing-library/react';
-import '@testing-library/jest-dom/vitest';
+import { cleanup, render, screen, waitFor } from '@testing-library/react';
 import { BrowserRouter } from 'react-router-dom';
 import ProtectedRoute from '../Routes/ProtectedRoute';
+import * as AuthHook from '../Hooks/Auth/useCheckSession';
 
 vi.mock('../Hooks/Auth/useCheckSession', () => ({
-  useCheckSession: () => ({ isLoading: true, isError: false }),
+  useCheckSession: vi.fn(),
 }));
 
+const mockedUseCheckSession = AuthHook.useCheckSession as any;
+
 describe('ProtectedRoute', () => {
-  it('renders LoadingPage when session is being checked', () => {
+  beforeEach(() => {
+    vi.resetAllMocks(); // Ensures mocks are reset before each test
+    cleanup(); // Clean up the DOM after each test
+  });
+
+  it('redirects to login when there is an error', async () => {
+    // Mocking an error scenario for this specific test
+    mockedUseCheckSession.mockImplementation(() => ({
+      isLoading: false,
+      isError: true,
+      data: undefined,
+      error: undefined,
+    }));
+
     render(
       <BrowserRouter>
         <ProtectedRoute />
       </BrowserRouter>
     );
-    const spinner = screen.getByRole('progressbar');
-    const text = screen.getByText('Loading...');
 
-    expect(spinner).toBeInTheDocument();
-    expect(text).toBeInTheDocument();
+    await waitFor(() => {
+      expect(window.location.pathname).toBe('/auth/login');
+    });
+  });
+
+  it('renders LoadingPage when session is being checked', () => {
+    // Mocking a loading scenario for this test
+    mockedUseCheckSession.mockImplementation(() => ({
+      isLoading: true,
+      isError: false,
+      data: undefined,
+      error: undefined,
+    }));
+
+    render(
+      <BrowserRouter>
+        <ProtectedRoute />
+      </BrowserRouter>
+    );
+
+    expect(screen.getByRole('progressbar')).toBeInTheDocument();
+    expect(screen.getByText('Loading...')).toBeInTheDocument();
   });
 });
